be97aa99fefa55a8fd4af0f9dc5816ac363dbf3a
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2023-11-24 17:05:45 +0200

    Tehtävä 1.4

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -19,9 +19,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.p1} ex_count={props.ex1}/>
-      <Part name={props.p2} ex_count={props.ex2}/>
-      <Part name={props.p3} ex_count={props.ex3}/>
+      <Part name={props.parts[0].name} ex_count={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} ex_count={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} ex_count={props.parts[2].exercises}/>
     </div>
   )
 }
@@ -37,24 +37,27 @@ const Total = (props) => {
 const App = () => {
   // const-määrittelyt
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
 
   return (
     <div>
       <Header course={course} />
-      <Content p1={part1.name} ex1={part1.exercises} p2={part2.name} ex2={part2.exercises} p3={part3.name} ex3={part3.exercises} />
-      <Total total_count={part1.exercises+part2.exercises+part3.exercises}/>
+      <Content parts={parts} />
+      <Total parts={parts}/>
     </div>
   )
 }
