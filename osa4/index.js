7a963193a1a54222e1dee2c27885cc8a0604f384
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-07-24 18:37:31 +0300

    App ja Index ovat nyt erikseen (4.2)

--- osa4/index.js
+++ osa4/index.js
@@ -1,26 +1,6 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
-const config = require("./utils/config")
-const logger = require("./utils/logger")
-const blogsRouter = require("./controllers/blogs")
-
-logger.info(`connecting to ${config.MONGODB_URI}`)
-mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info("connected to MongoDB")
-    })
-    .catch((error) => {
-        logger.error("error connecting to MongoDB:", error.message)
-    })
-mongoose.set("strictQuery", false)
-
-app.use(cors())
-app.use(express.json())
-
-app.use("/api/blogs", blogsRouter)
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
