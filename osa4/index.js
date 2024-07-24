46a1af94784c88c05c59d1ea11002f08838559a5
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-07-24 18:12:55 +0300

    Siirretty mongodb connection indexiin (4.2)

--- osa4/index.js
+++ osa4/index.js
@@ -2,6 +2,18 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const mongoose = require("mongoose")
+
+const url = process.env.MONGODB_URI
+console.log("connecting to", url)
+mongoose.connect(url)
+    .then(() => {
+        console.log("connected to MongoDB")
+    })
+    .catch((error) => {
+        console.log("error connecting to MongoDB:", error.message)
+    })
+mongoose.set("strictQuery", false)
 
 const Blog = require("./models/blog")
 
--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -1,19 +1,5 @@
 const mongoose = require("mongoose")
 
-const url = process.env.MONGODB_URI
-
-console.log("connecting to", url)
-
-mongoose.connect(url)
-    .then(() => {
-        console.log("connected to MongoDB")
-    })
-    .catch((error) => {
-        console.log("error connecting to MongoDB:", error.message)
-    })
-
-mongoose.set("strictQuery", false)
-
 const blogSchema = mongoose.Schema({
     title: String,
     author: String,
