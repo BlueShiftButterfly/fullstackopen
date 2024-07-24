90938345c32c01073e772f28deb06962282bd8e4
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-07-24 18:32:33 +0300

    Controller ja logger ovat omissa moduuleissa (4.2)

--- osa4/index.js
+++ osa4/index.js
@@ -4,40 +4,24 @@ const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
 const config = require("./utils/config")
+const logger = require("./utils/logger")
+const blogsRouter = require("./controllers/blogs")
 
-console.log("connecting to", config.MONGODB_URI)
+logger.info(`connecting to ${config.MONGODB_URI}`)
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
-        console.log("connected to MongoDB")
+        logger.info("connected to MongoDB")
     })
     .catch((error) => {
-        console.log("error connecting to MongoDB:", error.message)
+        logger.error("error connecting to MongoDB:", error.message)
     })
 mongoose.set("strictQuery", false)
-
-const Blog = require("./models/blog")
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/api/blogs", (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post("/api/blogs", (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use("/api/blogs", blogsRouter)
 
 app.listen(config.PORT, () => {
-    console.log(`Server running on port ${config.PORT}`)
+    logger.info(`Server running on port ${config.PORT}`)
 })
