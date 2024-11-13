ed7336a71288e2644d5e57f52ab759bc897ddd66
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-11-13 13:57:00 +0200

    Tehtävä 4.6

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -22,8 +22,29 @@ const favouriteBlog = (blogs) => {
         [0]
 }
 
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return null;
+    var authors = new Map()
+    blogs.forEach(blog => {
+        if (authors.has(blog.author) === false) {
+            authors.set(blog.author, 1)
+        }
+        else {
+            authors.set(blog.author, authors.get(blog.author) + 1)
+        }
+    })
+    var popularAuthor = Array.from(authors.entries()).sort
+    (
+        (a, b) => {
+            return b[1] - a[1]
+        }
+    )[0]    
+    return { author: popularAuthor[0], blogs: popularAuthor[1] }
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs
 }
