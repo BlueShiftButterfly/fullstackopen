6663c52cc7b90fc3a04408f0555c91c65cdcc035
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-11-13 12:48:01 +0200

    Tehtävä 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
     return 1
 }
+
+const totalLikes = (blogs) => {
+    return blogs.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.likes, 0
+    )
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
