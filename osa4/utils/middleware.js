90ee7c7808f8054c9bbc742b2bb1e4b42a531b95
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2025-04-07 17:03:06 +0300

    Tehtävä 4.20

--- osa4/utils/middleware.js
+++ osa4/utils/middleware.js
@@ -31,8 +31,18 @@ const errorHandler = (error, request, response, next) => {
     next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+    request.token = null
+    const authorization = request.get("authorization")
+    if (authorization && authorization.startsWith("Bearer ")) {
+        request.token = authorization.replace("Bearer ", "") 
+    }
+    next()
+}
+
 module.exports = {
     requestLogger,
     unknownEndpoint,
-    errorHandler
+    errorHandler,
+    tokenExtractor
 }
