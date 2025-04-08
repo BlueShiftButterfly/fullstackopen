7dd98cc4d3a44adf2078b94f7caff5966cdcfa94
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2025-04-08 16:00:28 +0300

    Tehtävä 4.22

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -2,42 +2,37 @@ const jwt = require("jsonwebtoken")
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 const User = require("../models/user")
+const middleware = require("../utils/middleware")
 
 blogsRouter.get("", async (request, response) => {
     const blogs = await Blog.find({}).populate("user", { username: 1, name: 1, id: 1 })
     response.json(blogs)
 })
 
-blogsRouter.post("", async (request, response) => {
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: "token invalid" }).end()
-    }
+blogsRouter.post("", middleware.userExtractor, async (request, response) => {
     const blog = new Blog(request.body)
     if (blog.title === undefined || blog.url === undefined) {
-        response.status(400).end()
-    } else {
-        if (blog.likes === undefined){
-            blog.likes = 0
-        }
-        const user = await User.findById(decodedToken.id)
-        if (user !== undefined) {
-            blog.user = user.id
-        }
-        const savedBlog = await blog.save()
-        user.blogs = user.blogs.concat(savedBlog.id)
-        await user.save()
-        response.status(201).json(blog)
+        return response.status(400).end()
+    } 
+    if (blog.likes === undefined){
+        blog.likes = 0
     }
+    blog.user = request.user.id
+    const savedBlog = await blog.save()
+    request.user.blogs = request.user.blogs.concat(savedBlog.id)
+    await request.user.save()
+    response.status(201).json(blog)
 })
 
-blogsRouter.delete("/:id", async (request, response) => {
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: "token invalid" }).end()
+blogsRouter.delete("/:id", middleware.userExtractor, async (request, response) => {
+    if (!request.user) {
+        return response.status(401).json({ error: "user not found" }).end()
     }
     const blog = await Blog.findById(request.params.id)
-    if (blog.user.toString() === decodedToken.id.toString()) {
+    if  (!blog) {
+        return response.status(404).end()
+    }
+    if (blog.user.toString() === request.user.id.toString()) {
         await Blog.findByIdAndDelete(request.params.id)
         response.status(204).end()
     } else {
