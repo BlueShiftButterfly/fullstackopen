e2ccba636c53a4f7921a523be71fd00ad9b684fe
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2025-04-15 16:55:52 +0300

    Tehtävä 5.2

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -15,6 +15,9 @@ const App = () => {
             const user = await loginService.login({
                 username, password,
             })
+            window.localStorage.setItem(
+                "loggedBlogappUser", JSON.stringify(user)
+            )
             setUser(user)
             setUsername("")
             setPassword("")
@@ -26,10 +29,24 @@ const App = () => {
         }
     }
 
+    const handleLogout = async (event) => {
+        event.preventDefault()
+        window.localStorage.removeItem("loggedBlogappUser")
+        setUser(null)
+    }
+
     useEffect(() => {
         blogService.getAll().then(blogs =>
         setBlogs( blogs )
         )  
+    }, [])
+
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser")
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON)
+            setUser(user)
+        }
     }, [])
 
     if (user === null) {
@@ -55,6 +72,7 @@ const App = () => {
         <div>
         <h2>blogs</h2>
         <p>{user.name} logged in</p>
+        <button onClick={handleLogout}>logout</button>
         {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
         )}
