b400e5b73ea5392c8879d51a56711d46fbbaa6b2
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2025-05-12 13:49:42 +0300

    Tehtävä 7.4

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -6,6 +6,7 @@ import {
     useMatch,
     useNavigate
 } from "react-router-dom"
+import { useField } from "./hooks"
 
 const Menu = () => {
     const padding = {
@@ -75,17 +76,16 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState("")
-    const [author, setAuthor] = useState("")
-    const [info, setInfo] = useState("")
-
+    const content = useField("text")
+    const author = useField("text")
+    const info = useField("text")
 
     const handleSubmit = (e) => {
         e.preventDefault()
         props.addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0
         })
     }
@@ -96,15 +96,15 @@ const CreateNew = (props) => {
             <form onSubmit={handleSubmit}>
                 <div>
           content
-                    <input name="content" value={content} onChange={(e) => setContent(e.target.value)} />
+                    <input type={content.type} name="content" value={content.value} onChange={content.onChange} />
                 </div>
                 <div>
           author
-                    <input name="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+                    <input type={author.type} name="author" value={author.value} onChange={author.onChange} />
                 </div>
                 <div>
           url for more info
-                    <input name="info" value={info} onChange={(e)=> setInfo(e.target.value)} />
+                    <input type={info.type} name="info" value={info.value} onChange={info.onChange} />
                 </div>
                 <button>create</button>
             </form>
